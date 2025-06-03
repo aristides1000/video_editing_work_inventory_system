97c2299740ee9c5f091e463f0011bb98443223cc
aristides1000 <aristides1000@gmail.com>
2025-06-03 18:34:28 +0000

    Finish structure of the inventory system

--- db_structure.sql
+++ db_structure.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS video_inventory_system;
+CREATE DATABASE IF NOT EXISTS video_inventory_system_TEST;
 
-USE video_inventory_system;
+USE video_inventory_system_TEST;
 
 CREATE TABLE IF NOT EXISTS types_of_user (
   id INT NOT NULL AUTO_INCREMENT,
@@ -34,7 +34,10 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (nickname, password, user_type_id)
-VALUES ("super usuario", md5('123'), 1);
+VALUES ("super usuario", md5('123'), 1),
+("administrador", md5('123'), 2),
+("inspector", md5('123'), 3),
+("responsable", md5('123'), 4);
 
 CREATE TABLE IF NOT EXISTS equipment_types (
   id INT NOT NULL AUTO_INCREMENT,
@@ -60,4 +63,50 @@ INSERT INTO equipments_status (name)
 VALUES ("operativo"),
 ("averiado");
 
+CREATE TABLE IF NOT EXISTS types_of_activities (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+INSERT INTO types_of_activities (name)
+VALUES ("pauta"),
+("prestamo");
+
+CREATE TABLE IF NOT EXISTS equipments (
+  id INT NOT NULL AUTO_INCREMENT,
+  equipment_category_id INT NOT NULL,
+  type_of_equipment_id INT NOT NULL,
+  equipment_status_id INT NOT NULL,
+  image_path VARCHAR(255),
+  qr_equipment_image VARCHAR(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY(equipment_category_id) REFERENCES equipment_categories(id),
+  FOREIGN KEY(type_of_equipment_id) REFERENCES equipment_types(id),
+  FOREIGN KEY(equipment_status_id) REFERENCES equipments_status(id)
+);
+
+INSERT INTO equipments (equipment_category_id, type_of_equipment_id, equipment_status_id, image_path, qr_equipment_image)
+VALUES (1, 1, 1, "./equipment_image/default_image.png", "./qr_equipment_image/default_qr.png"),
+(2, 2, 2, "./equipment_image/default_image.png", "./qr_equipment_image/default_qr.png"),
+(1, 2, 1, "./equipment_image/default_image.png", "./qr_equipment_image/default_qr.png");
+
+CREATE TABLE IF NOT EXISTS warehouses (
+  id INT NOT NULL AUTO_INCREMENT,
+  equipment_id INT NOT NULL,
+  in_the_warehouse BOOLEAN NOT NULL,
+  date TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  type_of_activity_id INT NOT NULL,
+  activity VARCHAR(255) NOT NULL,
+  responsible_id INT NOT NULL,
+  verified_by_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(equipment_id) REFERENCES equipments(id),
+  FOREIGN KEY(type_of_activity_id) REFERENCES types_of_activities(id),
+  FOREIGN KEY(responsible_id) REFERENCES users(id),
+  FOREIGN KEY(verified_by_id) REFERENCES users(id)
+);
+
+INSERT INTO warehouses (equipment_id, in_the_warehouse, type_of_activity_id, activity, responsible_id, verified_by_id)
+VALUES (1, 1, 1, "video del parque", 4, 3),
+(2, 0, 2, "sesion en la playa", 4, 3);
