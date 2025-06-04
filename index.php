4ae238499967833d0370a67aa6ac5d4a947b1e1a
aristides1000 <aristides1000@gmail.com>
2025-06-04 15:04:55 +0000

    Feat: Put the basics includes in our system to all pages

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  include_once('./includes/header.php');
+?>
+  <title>Index Page</title>
+<?php
+  include_once('./includes/navbar.php');
+?>
+
+  <h1>Hello, world!</h1>
+<?php
+  include_once('./includes/footer.php');
+?>
