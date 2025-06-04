9bec697cbf912fb380471ceec63d6f568316fb1e
aristides1000 <aristides1000@gmail.com>
2025-06-04 15:12:53 +0000

    Feat: Add navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,2 +1,18 @@
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./index.php">Sistema de Inventarios de equipos</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Listado de equipos</a>
+          <a class="nav-link" href="#">Registro de usuarios</a>
+          <a class="nav-link" href="#">Retiro de equipos</a>
+          <a class="nav-link disabled" aria-disabled="true">Entrega de equipos</a>
+        </div>
+      </div>
+    </div>
+  </nav>
