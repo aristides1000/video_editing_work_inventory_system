652d7a6f5c8ac84115817e562285270ca4ad4cf3
aristides1000 <aristides1000@gmail.com>
2025-06-19 15:46:13 +0000

    Feat: Create enviroment to production

--- conexion.php
+++ conexion.php
@@ -1,5 +1,10 @@
 <?php
-  $link = mysqli_connect('localhost','root','','video_inventory_system_TEST');
+  require __DIR__ . "/config.php";
+
+  $link = mysqli_connect(DATABASE_HOSTNAME,
+                          DATABASE_USERNAME,
+                          DATABASE_PASSWORD,
+                          DATABASE_NAME);
 
   if (!$link) {
     die('Error de Conexión: (' . mysqli_connect_errno() . ') '
