51559492365280bbf373aae0294c838ade0fe2ef
aristides1000 <aristides1000@gmail.com>
2025-06-19 16:00:41 +0000

    Feat: Create enviroment to production

--- conexion.php
+++ conexion.php
@@ -1,10 +1,7 @@
 <?php
   require __DIR__ . "/config.php";
 
-  $link = mysqli_connect(DATABASE_HOSTNAME,
-                          DATABASE_USERNAME,
-                          DATABASE_PASSWORD,
-                          DATABASE_NAME);
+  $link = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
 
   if (!$link) {
     die('Error de Conexión: (' . mysqli_connect_errno() . ') '
