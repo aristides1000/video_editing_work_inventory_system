5fe5d41fed8be5bf6a79710724b7a2ecd4006fd5
aristides1000 <aristides1000@gmail.com>
2025-07-08 20:52:33 +0000

    Feat finish with issues in the same data sended

--- update_equipment_warehouse_logic.php
+++ update_equipment_warehouse_logic.php
@@ -1,4 +1,6 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
   session_start();
   include 'conexion.php';
   include_once('./includes/header.php');
@@ -26,37 +28,100 @@
                               AND w.date < w2.date
                               AND w2.is_deleted = 0
                             WHERE
-                              e.id = 7
+                              e.id = ". $_POST['id_equipment'] ."
                               AND w2.id IS NULL;";
     $queryEquipmentSelected = mysqli_query($link, $sqlEquipmentSelected);
-    while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-      if ($_POST['equipment_category_id'] == $row[] && !isset($_FILES['image_path']['name'])) {
-
+    while($row = mysqli_fetch_array($queryEquipmentSelected, MYSQLI_ASSOC)) {
+      /* inicio test */
+      echo $_POST['id_equipment'] . " " . $row['id_equipment'] . "<br>";
+      echo $_POST['equipment_category_id'] . " " . $row['equipment_category_id'] . "<br>";
+      echo $_POST['type_of_equipment_id'] . " " . $row['type_of_equipment_id'] . "<br>";
+      echo $_POST['equipment_status_id'] . " " . $row['equipment_status_id'] . "<br>";
+      echo $_POST['equipment_is_deleted'] . " " . $row['equipment_is_deleted'] . "<br>";
+      echo $_POST['warehouse_id'] . " " . $row['warehouse_id'] . "<br>";
+      echo $_POST['type_of_activity_id'] . " " . $row['type_of_activity_id'] . "<br>";
+      echo $_POST['activity'] . " " . $row['activity'] . "<br>";
+      echo $_POST['responsible_id'] . " " . $row['responsible_id'] . "<br>";
+      echo $_POST['verified_by_id'] . " " . $row['verified_by_id'] . "<br>";
+      echo $_POST['warehouse_is_deleted'] . " " . $row['warehouse_is_deleted'] . "<br>";
+      if (isset($_FILES['image_path']['name'])) {
+        echo 'si foto';
+      } else {
+        echo "no foto";
+      }
+      /* fin test */
+      if ($_POST['id_equipment'] == $row['id_equipment'] && $_POST['equipment_category_id'] == $row['equipment_category_id'] && $_POST['type_of_equipment_id'] == $row['type_of_equipment_id'] && $_POST['equipment_status_id'] == $row['equipment_status_id'] && $_POST['equipment_is_deleted'] == $row['equipment_is_deleted'] && $_POST['warehouse_id'] == $row['warehouse_id'] && $_POST['type_of_activity_id'] == $row['type_of_activity_id'] && $_POST['activity'] == $row['activity'] && $_POST['responsible_id'] == $row['responsible_id'] && $_POST['verified_by_id'] == $row['verified_by_id'] && $_POST['warehouse_is_deleted'] == $row['warehouse_is_deleted'] && isset($_FILES['image_path']['name']) && $_FILES['image_path']['name']!='') {
+        ?>
+          <div class="modal fade" id="sameData" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="staticBackdropLabel">Datos enviados iguales a los de la base de datos</h1>
+                  <a href="./update_equipment_warehouse.php?id_equipment=<?php echo $_POST['id_equipment'] ?>" class="ms-auto">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </a>
+                </div>
+                <div class="modal-body">
+                  Los Datos ingresados son iguales a los datos provenientes del equipo, por favor ingrese toda la informacion solicitada
+                </div>
+                <div class="modal-footer">
+                  <a href="./update_equipment_warehouse.php?id_equipment=<?php echo $_POST['id_equipment'] ?>">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+          <script>
+            const sameData = new bootstrap.Modal(document.getElementById('sameData'));
+            sameData.show();
+          </script>
+        <?php
+        break;
       }
     }
     # Actualizar los queries
 
-    $sql_equipments = "INSERT INTO equipments (equipment_category_id, type_of_equipment_id, equipment_status_id, image_path, qr_equipment_image, is_deleted)
-      VALUES ('$_POST[equipment_category_id]', '$_POST[type_of_equipment_id]', '$_POST[equipment_status_id]', '". $equipmentImageName ."', '". $qrEquipmentImageName ."', 0);";
+    $sql_equipments = "UPDATE equipments
+                        SET
+                          equipment_category_id = $_POST[equipment_category_id],
+                          type_of_equipment_id = $_POST[type_of_equipment_id],
+                          equipment_status_id = $_POST[equipment_status_id],
+                          is_deleted = $_POST[equipment_is_deleted]
+                        WHERE
+                          id = $_POST[id_equipment];";
     mysqli_query($link, $sql_equipments);
 
-    $sql_warehouses = "INSERT INTO warehouses (equipment_id, in_the_warehouse, type_of_activity_id, activity, responsible_id, verified_by_id, is_deleted)
-      VALUES (". intval($next_id) .", 1, 3, 'almacenado', ". $_SESSION['id'] .", ". $_SESSION['id'] .", 0)";
+    $sql_warehouses = "INSERT INTO warehouses (equipment_id,
+                                                in_the_warehouse,
+                                                type_of_activity_id,
+                                                activity,
+                                                responsible_id,
+                                                verified_by_id,
+                                                is_deleted)
+                        VALUES ($_POST[id_equipment],
+                                $_POST[in_the_warehouse],
+                                $_POST[type_of_activity_id],
+                                '$_POST[activity]',
+                                $_POST[responsible_id],
+                                $_POST[verified_by_id],
+                                $_POST[warehouse_is_deleted])";
     mysqli_query($link, $sql_warehouses);
 
     if (mysqli_error($link)) {
       ?>
-        <div class="modal fade" id="equipmentRegistrationError" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal fade" id="equipmentUpdateError" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Error al Registrar</h1>
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Error al Modificar</h1>
                 <a href="./equipment_registration.php" class="ms-auto">
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </a>
               </div>
               <div class="modal-body">
-                Error en el registro del equipo. Por Favor intente de nuevo.
+                Error en la modificacion del equipo. Por Favor intente de nuevo.
               </div>
               <div class="modal-footer">
                 <a href="./equipment_registration.php">
@@ -68,8 +133,8 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
         <script>
-          const equipmentRegistrationError = new bootstrap.Modal(document.getElementById('equipmentRegistrationError'));
-          equipmentRegistrationError.show();
+          const equipmentUpdateError = new bootstrap.Modal(document.getElementById('equipmentUpdateError'));
+          equipmentUpdateError.show();
         </script>
       <?php
     } else { # Si no da error la insercion;
@@ -110,17 +175,9 @@
           <?php
         } else { # el tipo de archivo es correcto se efectua la carga del producto
           if ($tipo !== false) { # Si la foto ha sido cargada
-            $result = mysqli_query($link, "SHOW CREATE TABLE equipments");
-            $row = mysqli_fetch_assoc($result);
-            $create_table = $row['Create Table'];
-
-            // Extraer el valor AUTO_INCREMENT
-            preg_match('/AUTO_INCREMENT=(\d+)/', $create_table, $matches);
-            $next_id = $matches[1] ?? null;
-
             # nombres de las imagenes de los equipos y los qr de los equipos
             $imageExtention = ($tipo[2] == 1) ? '.gif' : (($tipo[2] == 2) ? '.jpg' : '.png');
-            $equipmentImageName = 'equipment-image-' . $next_id . $imageExtention;
+            $equipmentImageName = 'equipment-image-' . $_POST['id_equipment'] . $imageExtention;
 
             # guardo la imagen del equipo en servidor
             $dir_equipment_image = './equipment_image/';
